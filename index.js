fe9a4f08d4446c316ab4073764600cffbee946ed
Yasmin Castillo <yasmin_c@outlook.com>
2023-07-05 19:56:51 -0500

    added questions to array

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["what did you create?","What is the name of your project?", "What languages did you use?","What was the goal?","Is there any installition required?","What is the link to the gihub pages?","How do you use this program/website?","List your credits","What Licenses do you have?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
